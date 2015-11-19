91440f2ad1a9de30ae927053fa92e5ccb046a5e6
Sebastián Peyrott <sebastian.peyrott@auth0.com>
2015-11-19 05:00:36 -0300

    Initial import.

---
+++ driver.js
@@ -0,0 +1,95 @@
+var dirs = {
+	react: 'react',
+	ember: 'ember/dist',
+	ember2: 'ember2/dist',
+	incrementaldom: 'incrementaldom'
+}
+
+var browserPerf = require('browser-perf');
+var spawn = require('child_process').spawn;
+
+var FILE = 'data.json';
+var fs = require('fs');
+if (!fs.existsSync(FILE)) {
+	fs.writeFileSync(FILE, JSON.stringify({}));
+}
+
+function startServer(dir) {
+    var fulldir = process.cwd() + '/' + dir;
+    
+    console.log("Starting Python HTTP server at " + fulldir);
+
+    return spawn('python2', ['-m', 'SimpleHTTPServer'], {
+        cwd: fulldir
+    });
+}
+
+function stopServer(childProcess) {
+    console.log("Stopping Python HTTP server");
+
+    childProcess.kill('SIGTERM');
+}
+
+var frameworks = Object.keys(dirs);
+(function runTest(i) {
+	if (i >= frameworks.length) {
+		console.log('All tests done');
+		return;
+	}
+
+    console.log("Starting Python HTTP server at " + 
+		    process.cwd() + '/' + dirs[frameworks[i]]);
+		    
+	var child = startServer(dirs[frameworks[i]]);
+	setTimeout(function() {
+        console.log('Starting benchmark...');
+        repeatTest(frameworks[i], function() {
+            stopServer(child);
+            runTest(i + 1);
+        });
+    }, 1000);
+	
+	child.on('error', function(err) {
+	    console.log("Fatal error: ");
+	    console.log(err);
+	    process.exit(1);
+	});
+}(0));
+
+function repeatTest(framework, cb) {
+	var REPEAT = 1;
+	console.log('Running test for %s', framework);
+	(function iterate(i) {
+		if (i >= REPEAT) {
+			console.log('All tests done for %s', framework);
+			cb();
+			return;
+		}				
+
+		console.log('[%d|%d]', i, REPEAT);
+		browserPerf('http://localhost:8000', function(err, result) {
+			if (err) {
+				console.error(err);
+			} else {
+				var data = JSON.parse(fs.readFileSync(FILE));
+				if (typeof data[framework] === 'undefined') {
+					data[framework] = {};
+				}
+				result.forEach(function(res) {
+					for (var metric in res) {
+						if (typeof data[framework][metric] === 'undefined') {
+							data[framework][metric] = [];
+						}
+						data[framework][metric].push(res[metric]);
+					}
+				});
+				fs.writeFileSync(FILE, JSON.stringify(data));
+			}
+			iterate(i + 1);
+		}, {
+			selenium: 'http://localhost:9515',
+			browsers: ['chrome']
+		});
+
+	}(0));
+}
---
+++ incrementaldom/script.js
@@ -0,0 +1,190 @@
+var elementOpen = IncrementalDOM.elementOpen,
+    elementClose = IncrementalDOM.elementClose,
+    elementVoid = IncrementalDOM.elementVoid,
+    text = IncrementalDOM.text,
+    patch = IncrementalDOM.patch;
+
+var ENV = {};
+ENV.rows = 100;
+ENV.timeout = 0;
+
+var start = Date.now();
+var loadCount = 0;
+
+function getData() {
+  // generate some dummy data
+  data = {
+    start_at: new Date().getTime() / 1000,
+    databases: {}
+  };
+
+  for (var i = 1; i <= ENV.rows; i++) {
+    data.databases["cluster" + i] = {
+      queries: []
+    };  
+
+    data.databases["cluster" + i + "slave"] = {
+      queries: []
+    };
+  }
+
+  Object.keys(data.databases).forEach(function(dbname) {
+    var info = data.databases[dbname];
+
+    var r = Math.floor((Math.random() * 10) + 1);
+    for (var i = 0; i < r; i++) {
+      var q = {
+        canvas_action: null,
+        canvas_context_id: null,
+        canvas_controller: null,
+        canvas_hostname: null,
+        canvas_job_tag: null,
+        canvas_pid: null,
+        elapsed: Math.random() * 15,
+        query: "SELECT blah FROM something",
+        waiting: Math.random() < 0.5
+      };
+
+      if (Math.random() < 0.2) {
+        q.query = "<IDLE> in transaction";
+      }
+
+      if (Math.random() < 0.1) {
+        q.query = "vacuum";
+      }
+
+      info.queries.push(q);
+    }
+
+    info.queries = info.queries.sort(function(a, b) {
+      return b.elapsed - a.elapsed;
+    });
+  });
+
+  return data;
+}
+
+var _base;
+
+(_base = String.prototype).lpad || (_base.lpad = function(padding, toLength) {
+  return padding.repeat((toLength - this.length) / padding.length).concat(this);
+});
+
+function formatElapsed(value) {
+  str = parseFloat(value).toFixed(2);
+  if (value > 60) {
+    minutes = Math.floor(value / 60);
+    comps = (value % 60).toFixed(2).split('.');
+    seconds = comps[0].lpad('0', 2);
+    ms = comps[1];
+    str = minutes + ":" + seconds + "." + ms;
+  }
+  return str;
+}
+
+function renderQuery(query) {
+    var className = "elapsed short";
+    if (query.elapsed >= 10.0) {
+        className = "elapsed warn_long";
+    } else if (query.elapsed >= 1.0) {
+        className = "elapsed warn";
+    }
+
+    elementOpen('td', null, null, 'class', className);
+        text(query.elapsed ? formatElapsed(query.elapsed) : '');
+        elementOpen('div', null, ['class', 'popover left']);
+            elementOpen('div', null, ['class', 'popover-content']);
+                text(query.query);
+            elementClose('div');
+            elementVoid('div', null, ['class', 'arrow']);
+        elementClose('div');
+    elementClose('td');
+}
+
+function renderSample(queries, time) {
+    var topFiveQueries = queries.slice(0, 5);
+    while (topFiveQueries.length < 5) {
+        topFiveQueries.push({ query: "" });
+    }
+
+    var countClassName = "label";
+    if (queries.length >= 20) {
+        countClassName += " label-important";
+    } else if (queries.length >= 10) {
+        countClassName += " label-warning";
+    } else {
+        countClassName += " label-success";
+    }
+
+    elementOpen('td', null, ['class', 'query-count']);
+        elementOpen('span', null, null, 'class', countClassName);
+          text(queries.length);
+        elementClose('span');
+    elementClose('td');
+
+    topFiveQueries.forEach(renderQuery);
+}
+
+function renderDatabase(db, samples, idx) {
+    var lastSample = samples[samples.length - 1];
+
+    elementOpen('tr');
+        elementOpen('td', idx, ['class', 'dbname']);
+            text(db);
+        elementClose('td');        
+        renderSample(lastSample.queries, lastSample.time);
+    elementClose('tr');        
+}
+
+function renderDBMon(databases) {
+    elementOpen('div');
+        elementOpen('table', null, ['class', 'table table-striped latest-data']);
+            elementOpen('tbody');
+                
+                Object.keys(databases).forEach(function(db, idx) {
+                    renderDatabase(db, databases[db].samples, idx);
+                });
+                
+            elementClose('tbody');
+        elementClose('table');
+    elementClose('div');
+}
+
+var globals = {
+    databases: {},
+    loadCount: 0
+};
+
+function loadSamples() {
+    globals.loadCount++;
+    var newData = getData();
+
+    Object.keys(newData.databases).forEach(function(dbname) {
+      var sampleInfo = newData.databases[dbname];
+
+      if (!globals.databases[dbname]) {
+        globals.databases[dbname] = {
+          name: dbname,
+          samples: []
+        }
+      }
+
+      var samples = globals.databases[dbname].samples;
+      samples.push({
+        time: newData.start_at,
+        queries: sampleInfo.queries
+      });
+      if (samples.length > 5) {
+        samples.splice(0, samples.length - 5);
+      }
+    });
+
+    patch(document.getElementById('dbmon'), renderDBMon, globals.databases);
+    
+    setTimeout(this.loadSamples, ENV.timeout);
+}
+
+// Start doing stuff
+loadSamples();
+
+
